ebc82f9fac46b303b37f5ad398244ce263c51629
Mona Helness <monaheln@stud.ntnu.no>
2024-02-12 15:45:01 +0100

    Style: Dashboard
    -Navbar added
    - Listview added
    - Logo + header added
    
    Related #5
    
    Co-authored-by: Philip <philipvl@stud.ntnu.no>

--- frontend/src/Components/Navbar/Navbar.tsx
+++ frontend/src/Components/Navbar/Navbar.tsx
@@ -3,14 +3,25 @@ import "./Navbar.css"
 
 import navbarIcon from "./icon/navbar.png"; // Path to your navbar icon
 import logoImage from "./icon/Icebreaker-4.png"; // Path to your other image
+import loginn from "./icon/loginn.png"; // Path to your other image
 
 const Navbar = () => {
   return (
     <div className="header">
       {/* Add inline style for margin-top to the navbar icon */}
-      <img src={navbarIcon} height="60" width="45" alt="Navbar Icon" style={{ marginTop: '8px' }} />
+      <div >
+      <img src={navbarIcon} height="70" width="45" alt="Navbar Icon" style={{ marginTop: '8px', marginLeft: '32px' }} />
+      </div>
+      <div >
       <img src={logoImage} height="100" width="400" alt="Logo" />
+      </div>
+      <div style={{display: 'flex', justifyContent: 'flex-end'}} >
+      <img src={loginn} height = "60" alt="Logo" style={{ marginTop: '16px'}}/>
+      </div>
+      
     </div>
+    
+    
   );
 };
 
